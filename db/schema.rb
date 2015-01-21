766d5b2a3ee8ae7d9a837801f2fe8c4ce6bcd191
Marcin Drozd <marcindrozd@me.com>
2015-01-21 17:54:44 +0100

    Create users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121161433) do
+ActiveRecord::Schema.define(version: 20150121165213) do
 
   create_table "posts", force: true do |t|
     t.string "url"
     t.string "title"
     t.text   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
   end
 
 end

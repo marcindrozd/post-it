356fd010c3a6c590f47b6d8cdcc30ca90d221008
Marcin Drozd <marcindrozd@me.com>
2015-01-27 15:35:44 +0100

    Complete post actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,48 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :show]
+
   def index
     @posts = Post.all
   end
 
   def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first # Update when adding admin part
+
+    if @post.save
+      flash[:notice] = "Post successfully created!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Post successfully updated!"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
     @post = Post.find(params[:id])
   end
 end

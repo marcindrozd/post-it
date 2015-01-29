bbf491f0afdb377be6819c949138fdfaa931703d
Marcin Drozd <marcindrozd@me.com>
2015-01-29 10:50:56 +0100

    Format urls and dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def updated_url(url)
+    url.start_with?("http") ? url : "http://#{url}"
+  end
+
+  def format_date(date)
+    date.strftime("%d-%b-%Y %H:%M")
+  end
 end

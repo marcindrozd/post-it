a6fa14264d2c107b65ac5e138592bd4d89f8e56a
Marcin Drozd <marcindrozd@me.com>
2015-02-13 12:15:22 +0100

    Fix for unique slugs for post titles

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :votes, as: :voteable
 
-  validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5 }
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
 
@@ -24,7 +24,29 @@ class Post < ActiveRecord::Base
   end
 
   def add_slug
-    self.slug = self.title.gsub(/[ \W]/, ' ' => "-", '\W' => "").downcase
+    a_slug = prepare_slug(self.title)
+    post = Post.find_by(slug: a_slug)
+    count = 1
+    while post && post != self
+      a_slug = append_suffix(a_slug, count)
+      post = Post.find_by(slug: a_slug)
+      count += 1
+    end
+    self.slug = a_slug
+  end
+
+  def prepare_slug(str)
+    str = str.strip
+    str.gsub!(/[ \W]/, ' ' => "-", '\W' => "").downcase
+    str
+  end
+
+  def append_suffix(str, count)
+    if str.split("-").last.to_i != 0
+      str.split("-").slice(0...-1).join("-") + "-" + count.to_s
+    else
+      str + "-" + count.to_s
+    end
   end
 
   def to_param
